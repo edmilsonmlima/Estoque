874994c245791947e45eabeaede7eeac04329962
Edmilson <edmilsonmlima@hotmail.com>
2021-03-18 20:31:22 -0300

    Update

---
+++ Ideal.bat
@@ -0,0 +1,40 @@
+@echo off
+
+
+
+echo --------------- IdealFolha RH -------------------------
+
+echo
+
+echo -----------  Ideal Informatica -------------------------
+
+echo
+
+echo              Iniciando a copia
+
+echo -------------------------------------
+
+echo
+
+echo -------------------------------------
+
+echo -------------------------------------
+
+
+xcopy "\\10.52.0.201\Banco Firebird RH\IdealFolha\*.*" /s/d/y 
+
+
+echo .....................................
+
+echo -------------------------------------
+
+echo                copia concluida
+
+echo -------------------------------------
+
+echo
+
+start ideal.exe
+
+
+
